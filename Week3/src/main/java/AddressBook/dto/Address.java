ceadcc6dceab11a6d336c093cda45987ce4ab014
Cole Ogden <cogden14@gmail.com>
2021-08-24 18:00:01 -0500

    Add hash and equals overloaders to Address class for AddressBook

--- Week3/src/main/java/AddressBook/dto/Address.java
+++ Week3/src/main/java/AddressBook/dto/Address.java
@@ -1,5 +1,7 @@
 package AddressBook.dto;
 
+import java.util.Objects;
+
 
 
 public class Address {
@@ -56,5 +58,55 @@ public class Address {
 
     public void setZipcode(int zipcode) {
         this.zipcode = zipcode;
+    }
+   
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 89 * hash + Objects.hashCode(this.firstName);
+        hash = 89 * hash + Objects.hashCode(this.lastName);
+        hash = 89 * hash + Objects.hashCode(this.address);
+        hash = 89 * hash + Objects.hashCode(this.city);
+        hash = 89 * hash + Objects.hashCode(this.state);
+        hash = 89 * hash + Objects.hashCode(this.zipcode);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Address other = (Address) obj;
+        if (!Objects.equals(this.firstName, other.firstName)) {
+            return false;
+        }
+        if (!Objects.equals(this.lastName, other.lastName)) {
+            return false;
+        }
+        if (!Objects.equals(this.address, other.address)) {
+            return false;
+        }
+        if (!Objects.equals(this.city, other.city)) {
+            return false;
+        }
+        if (!Objects.equals(this.state, other.state)) {
+            return false;
+        }
+        if (!Objects.equals(this.zipcode, other.zipcode)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" + "firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city=" + city + ", state=" + state + ", zipcode=" + zipcode + '}';
     }
 }
